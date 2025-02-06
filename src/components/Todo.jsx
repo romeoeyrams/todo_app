01920bec6acc45abb6208a89023021764a7df35e
Romeo Gborgblorvor <colbby@gmail.com>
2025-02-06 13:27:40 -0400

    first commit on todo app. concept of mapping, props, calling props,  managing block of codes into components

---
+++ src/components/Todo.jsx
@@ -0,0 +1,23 @@
+function Todo(props) {
+  return (
+    <li className="todo stack-small">
+      <div className="c-cb">
+        <input type="checkbox" id={props.id} defaultChecked = {props.completed} />
+        <label htmlFor={props.id} className="todo-label">
+          {props.name}
+        </label>
+      </div>
+      <div className="btn-group">
+        <button type="button" className="btn">
+          Edit <span className="visually-hidden">{props.name}</span>
+        </button>
+
+        <button type="button" className="btn btn__danger">
+          Delete <span className="visually-hidden">{props.name}</span>
+        </button>
+      </div>
+    </li>
+  );
+}
+
+export default Todo;
